538f3335f1f9f999d3a2fbd779a4a3f735384161
artooth <artooth@gmail.com>
2018-11-13 19:03:26 -0600

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //Connect to MySQL
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 //Make connection 
 connection.connect(function (err) {
